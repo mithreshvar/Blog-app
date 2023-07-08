42402c1ef6c717c92b31fa490cf4790350b3193e
MaverickShroov <83708479+MaverickShroov@users.noreply.github.com>
2023-07-08 20:01:34 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Blog from './components/Blog'
+import Home from './components/Home'
+import Login from './components/Login'
+import Create from './components/Create'
+// import Login from './components/login'
 
 function App() {
 
     return (
-        <>
-            <Blog/>
-        </>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<Login isLogIn={true} />}/>
+            <Route path='/signup' element={<Login/>}/>
+            <Route path='/read/:id' element={<Blog />}/>
+            <Route path='/create' element={<Create />}/>
+        </Routes>
     )
 }
 
